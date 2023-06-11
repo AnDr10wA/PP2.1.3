bfe0518e09217795fd64dd548f8fd6fc223bfd67
AnDr10wA <45951252+AnDr10wA@users.noreply.github.com>
2023-06-11 20:11:11 +0300

    Init commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+ 
+    private String message = "Hello world!!";
+ 
+    public String getMessage() {
+        return message;
+    }
+ 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+     
+}
